e410d576ffa47e099bbbc1fe871e000c3e64e7aa
dgmedvedev <dg-medvedev@mail.ru>
2022-10-04 12:11:01 +0300

    add VideoCourses.moduleOne add VideoCourses.moduleTwo

---
+++ moduleOne/src/module-info.java
@@ -0,0 +1,3 @@
+module moduleOne {
+    exports package1;
+}
---
+++ moduleOne/src/package1/Test1.java
@@ -0,0 +1,7 @@
+package package1;
+
+public class Test1 {
+    public static void show() {
+        System.out.println("This is Test1");
+    }
+}
