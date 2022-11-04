a98effc23ff235277d0a5f959b8e13118e8087cc
dgmedvedev <dg-medvedev@mail.ru>
2022-11-04 19:17:28 +0300

    add lesson04.nested_classes.local_inner_class,anonymous_class (VideoCourse_02.Lessons)

---
+++ src/VideoCourse_02/Lessons/lesson04_nested_classes/local_inner_class/LocalInner1.java
@@ -0,0 +1,49 @@
+package VideoCourse_02.Lessons.lesson04_nested_classes.local_inner_class;
+
+public class LocalInner1 {
+    public static void main(String[] args) {
+        Math math = new Math();
+        math.getResult();
+    }
+}
+
+class Math {
+    public void getResult() {
+        class Division { // локальный класс виден только внутри метода
+            private int divisible;
+            private int divisor;
+
+            public int getDivisible() {
+                return divisible;
+            }
+
+            public void setDivisible(int divisible) {
+                this.divisible = divisible;
+            }
+
+            public int getDivisor() {
+                return divisor;
+            }
+
+            public void setDivisor(int divisor) {
+                this.divisor = divisor;
+            }
+
+            public int getQuotient() {
+                return divisible / divisor;
+            }
+
+            public int getRemainder() {
+                return divisible % divisor;
+            }
+        }
+
+        Division division = new Division();
+        division.setDivisible(21);
+        division.setDivisor(4);
+        System.out.println("Делимое = " + division.getDivisible());
+        System.out.println("Делитель = " + division.getDivisor());
+        System.out.println("Частное = " + division.getQuotient());
+        System.out.println("Остаток = " + division.getRemainder());
+    }
+}
