cb2a5f2ede17cdf317bda2e6f825d49cc42269bd
dgmedvedev <dg-medvedev@mail.ru>
2022-11-22 19:46:32 +0300

    add lesson07_multithreading.WaitNotifyExample (VideoCourse_02.Lessons)

---
+++ src/VideoCourse_02/Lessons/lesson07_multithreading/WaitNotifyExample.java
@@ -0,0 +1,73 @@
+package VideoCourse_02.Lessons.lesson07_multithreading;
+
+public class WaitNotifyExample {
+    public static void main(String[] args) {
+        Market market = new Market();
+        Producer producer = new Producer(market);
+        Consumer consumer = new Consumer(market);
+        Thread threadProducer = new Thread(producer);
+        Thread threadConsumer = new Thread(consumer);
+        threadProducer.start();
+        threadConsumer.start();
+    }
+}
+
+class Market {
+    private int breadCount = 0;
+
+    public synchronized void getBread() {
+        while (breadCount < 1) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        breadCount--;
+        System.out.println("Потребитель купил 1 хлеб");
+        System.out.println("Количество хлеба в магазине = " + breadCount);
+        notify();
+    }
+
+    public synchronized void putBread() {
+        while (breadCount >= 5) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        breadCount++;
+        System.out.println("Производитель добавил 1 хлеб");
+        System.out.println("Количество хлеба в магазине = " + breadCount);
+        notify();
+    }
+}
+
+class Producer implements Runnable {
+    Market market;
+
+    Producer(Market market) {
+        this.market = market;
+    }
+
+    public void run() {
+        for (int i = 0; i < 10; i++) {
+            market.putBread();
+        }
+    }
+}
+
+class Consumer implements Runnable {
+    Market market;
+
+    Consumer(Market market) {
+        this.market = market;
+    }
+
+    public void run() {
+        for (int i = 0; i < 10; i++) {
+            market.getBread();
+        }
+    }
+}
