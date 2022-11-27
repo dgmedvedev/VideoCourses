d4a795b71f8b02b4b854c5200920252c4f3c4d43
dgmedvedev <dg-medvedev@mail.ru>
2022-11-27 16:27:11 +0300

    add lesson07_multithreading.semaphore (VideoCourse_02.Lessons)

---
+++ src/VideoCourse_02/Lessons/lesson07_multithreading/semaphore/SemaphoreEx.java
@@ -0,0 +1,41 @@
+package VideoCourse_02.Lessons.lesson07_multithreading.semaphore;
+
+import java.util.concurrent.Semaphore;
+
+// 2 телефонные будки, 5 желающих позвонить
+public class SemaphoreEx {
+    public static void main(String[] args) {
+        Semaphore callBox = new Semaphore(2);
+
+        new Person("Dima", callBox);
+        new Person("Oleg", callBox);
+        new Person("Elena", callBox);
+        new Person("Ivan", callBox);
+        new Person("Semen", callBox);
+    }
+}
+
+class Person extends Thread {
+    String name;
+    private final Semaphore CALL_BOX;
+
+    public Person(String name, Semaphore CALL_BOX) {
+        this.name = name;
+        this.CALL_BOX = CALL_BOX;
+        this.start();
+    }
+
+    public void run() {
+        try {
+            System.out.println(name + " ждёт...");
+            CALL_BOX.acquire(); // заблокирует поток пока ресурс не будет доступен
+            System.out.println(name + " пользуется телефоном...");
+            sleep(2000);
+            System.out.println(name + " завершил звонок...");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            CALL_BOX.release(); // освободит ресурс (semaphore counter увеличится на 1)
+        }
+    }
+}
