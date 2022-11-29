649a9a8e5ba89e0783cb96d9946d37d1708cb1d7
dgmedvedev <dg-medvedev@mail.ru>
2022-11-29 23:01:41 +0300

    add lesson03_collection.thread_safe (VideoCourse_02.Lessons)

--- src/VideoCourse_02/Lessons/lesson03_collection/thread_safe/SynchronizedCollectionEx2.java
+++ src/VideoCourse_02/Lessons/lesson03_collection/thread_safe/SynchronizedCollectionEx2.java
@@ -1,6 +1,8 @@
 package VideoCourse_02.Lessons.lesson03_collection.thread_safe;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class SynchronizedCollectionEx2 {
     public static void main(String[] args) throws InterruptedException {
@@ -8,12 +10,16 @@ public class SynchronizedCollectionEx2 {
         for (int i = 0; i < 1000; i++) {
             arrayList.add(i);
         }
+        List<Integer> synchList = Collections.synchronizedList(arrayList);
         Runnable runnable1 = () -> {
-            for (Integer integer : arrayList) {
-                System.out.println(integer);
+            // чтобы обезопасить приложение от вызова Exception, необходимо целиком блокировать List на время перебора
+            synchronized (synchList) {
+                for (Integer integer : synchList) {
+                    System.out.println(integer);
+                }
             }
         };
-        Runnable runnable2 = () -> arrayList.remove(10);
+        Runnable runnable2 = () -> synchList.remove(10);
 
         Thread thread1 = new Thread(runnable1);
         Thread thread2 = new Thread(runnable2);
@@ -21,5 +27,6 @@ public class SynchronizedCollectionEx2 {
         thread2.start();
         thread1.join();
         thread2.join();
+        System.out.println(synchList);
     }
 }
