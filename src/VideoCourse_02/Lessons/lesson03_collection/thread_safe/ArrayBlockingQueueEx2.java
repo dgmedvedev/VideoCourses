f8531cc2a75fa85b2ef9579b56366adda105ce46
dgmedvedev <dg-medvedev@mail.ru>
2022-12-01 20:45:20 +0300

    add lesson03_collection.thread_safe.ArrayBlockingQueueEx1,2 (VideoCourse_02.Lessons)

---
+++ src/VideoCourse_02/Lessons/lesson03_collection/thread_safe/ArrayBlockingQueueEx2.java
@@ -0,0 +1,37 @@
+package VideoCourse_02.Lessons.lesson03_collection.thread_safe;
+
+import java.util.concurrent.ArrayBlockingQueue;
+
+public class ArrayBlockingQueueEx2 {
+    public static void main(String[] args) {
+        ArrayBlockingQueue<Integer> arrayBlockingQueue
+                = new ArrayBlockingQueue<>(4);
+
+        // Producer
+        new Thread(() -> {
+            int i = 0;
+            while (true) {
+                try {
+                    arrayBlockingQueue.put(++i);
+                    System.out.println("Producer add: " + i + " " + arrayBlockingQueue);
+                    Thread.sleep(3000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+
+        // Consumer
+        new Thread(() -> {
+            while (true) {
+                try {
+                    Integer j = arrayBlockingQueue.take();
+                    System.out.println("Consumer take " + j);
+                    Thread.sleep(9000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+    }
+}
