663c9fb44a8c4fea017176a5217ff955bac94285
dgmedvedev <dg-medvedev@mail.ru>
2022-12-02 20:57:05 +0300

    add lesson08 FileReader/Writer,BufferedRW (VideoCourse_02.Lessons)

--- src/VideoCourse_02/Lessons/lesson08_work_with_files/TryWithResourcesWriter.java
+++ src/VideoCourse_02/Lessons/lesson08_work_with_files/TryWithResourcesWriter.java
@@ -3,17 +3,16 @@ package VideoCourse_02.Lessons.lesson08_work_with_files;
 import java.io.FileWriter;
 import java.io.IOException;
 
-public class FileWriterEx {
+public class TryWithResourcesWriter {
     public static void main(String[] args) throws IOException {
-        String string = "Example!\n";
+        String string = "\nTryWithResourcesWriter!\n";
         String rubai = """
                 Много лет размышлял я над жизнью земной.
                 Непонятного нет для меня под луной.
                 Мне известно, что мне ничего не известно!
                 Вот последняя правда, открытая мной.""";
-        FileWriter writer = null; // объявляем здесь переменную, чтобы она была видна в блоке finally
-        try {
-            writer = new FileWriter("testFile.txt", true); // - без true, файл будет перезаписываться
+        // в скобках указывается ресурс, который необходимо закрыть по завершению программы
+        try (FileWriter writer = new FileWriter("testFile.txt", true)) {
             System.out.println("1-ый вариант записи в файл");
             writer.write(string);
             System.out.println("2-ой вариант записи в файл");
@@ -23,8 +22,6 @@ public class FileWriterEx {
             System.out.println("Done!");
         } catch (IOException e) {
             e.printStackTrace();
-        } finally {
-            writer.close();
         }
     }
 }
