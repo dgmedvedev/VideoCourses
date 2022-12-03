90a8b194de78a6de57f1a6f0267d722d31237be6
dgmedvedev <dg-medvedev@mail.ru>
2022-12-03 23:24:27 +0300

    add lesson08 FileInput/OutputStream (PictureCopyEx) (VideoCourse_02.Lessons)

---
+++ src/VideoCourse_02/Lessons/lesson08_work_with_files/PictureCopyEx.java
@@ -0,0 +1,20 @@
+package VideoCourse_02.Lessons.lesson08_work_with_files;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class PictureCopyEx {
+    public static void main(String[] args) {
+        try (FileInputStream inputStream = new FileInputStream("d:\\Multimedia\\Photo\\3.jpg");
+             FileOutputStream outputStream = new FileOutputStream("3.jpg")) {
+            int i;
+            while ((i = inputStream.read()) != -1) {
+                outputStream.write(i);
+            }
+            System.out.println("Done!");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
