dcfbc0c0f824310b5781e167d4df235903b9f27a
dgmedvedev <dg-medvedev@mail.ru>
2022-12-08 13:15:15 +0300

    add lesson08 ChannelBufferEx1 (VideoCourse_02.Lessons)

--- src/VideoCourse_02/Lessons/lesson08_work_with_files/channel_buffer/ChannelBufferEx1.java
+++ src/VideoCourse_02/Lessons/lesson08_work_with_files/channel_buffer/ChannelBufferEx1.java
@@ -9,6 +9,7 @@ public class ChannelBufferEx1 {
     public static void main(String[] args) {
         try (RandomAccessFile file = new RandomAccessFile("test10.txt", "rw");
              FileChannel channel = file.getChannel()) {
+// Чтение из файла
             ByteBuffer buffer = ByteBuffer.allocate(25);
             StringBuilder poem = new StringBuilder();
 
@@ -27,6 +28,20 @@ public class ChannelBufferEx1 {
                 byteRead = channel.read(buffer);
             }
             System.out.println(poem);
+
+// Запись в файл
+            String text = """
+                                        
+                    There are only two ways to live your life.
+                    One is as though nothing is a miracle.
+                    The other is as though everything is as though everything is a miracle.""";
+
+            // 1-ый способ записи в файл
+            ByteBuffer buffer2 = ByteBuffer.allocate(text.getBytes().length);
+            buffer2.put(text.getBytes());
+            buffer2.flip();
+            channel.write(buffer2);
+
         } catch (IOException e) {
             e.printStackTrace();
         }
