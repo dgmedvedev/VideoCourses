8579931206031c52b684ae2058ab601d77330726
dgmedvedev <dg-medvedev@mail.ru>
2022-12-14 13:15:07 +0300

    add lesson09_regex (VideoCourse_02.Lessons)

---
+++ src/VideoCourse_02/Lessons/lesson09_regex/Regex1.java
@@ -0,0 +1,15 @@
+package VideoCourse_02.Lessons.lesson09_regex;
+
+public class Regex1 {
+    public static void main(String[] args) {
+        String s
+                = "Ivanov Vasiliy, Russia, Moscow,Lenin street,51,Flat 48," +
+                "email:vivanov@mail.ru,Postcode: AA99,Phone Number: +123456789;" +
+                "Petrova Mariya, Belarus, Minsk,Pobeda street,123,Flat 18," +
+                "email:masha@tut.by,Postcode: BEL54,Phone Number: +987654321;" +
+                "Chuck Norris, USA, Hollywood,All stars street,87,Flat 21," +
+                "email:chuck@gmail.com,Postcode: USA23,Phone Number: +136478952.";
+
+        System.out.println(s);
+    }
+}
---
+++ src/VideoCourse_02/Lessons/lesson09_regex/Regex2.java
@@ -0,0 +1,9 @@
+package VideoCourse_02.Lessons.lesson09_regex;
+
+public class Regex2 {
+    public static void main(String[] args) {
+        String s1 = "ABCD ABCE ABCFABCDABCH";
+
+        System.out.println(s1);
+    }
+}
