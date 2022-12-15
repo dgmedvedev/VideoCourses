c90abdfd5e1d22e1415f286a8279392062647160
dgmedvedev <dg-medvedev@mail.ru>
2022-12-15 13:18:07 +0300

    add lesson09_regex.Regex2 (VideoCourse_02.Lessons)

--- src/VideoCourse_02/Lessons/lesson09_regex/Regex2.java
+++ src/VideoCourse_02/Lessons/lesson09_regex/Regex2.java
@@ -1,9 +1,43 @@
 package VideoCourse_02.Lessons.lesson09_regex;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Regex2 {
     public static void main(String[] args) {
-        String s1 = "ABCD ABCE ABCFABCDABCH";
+        String s = "ABD5KP ABE7KP AB7KP AB8KP";
 
-        System.out.println(s1);
+        Pattern pattern1 = Pattern.compile("AB8K"); // - соответствует последовательно идущим символам
+        Pattern pattern2 = Pattern.compile("[D78]"); // - соответствие одному любому символу из квадратных скобок
+        Pattern pattern3 = Pattern.compile("AB[C-H][3-7]KP"); // - соответствие одному символу из диапазона
+        Pattern pattern4 = Pattern.compile("AB[C-H3-7]KP"); // - соответствие одному символу из ДИАПАЗОНОВ
+        Pattern pattern5 = Pattern.compile("AB[^C-H3-7]KP"); // - знак ^ - соответствие одному символу НЕ из ДИАПАЗОНОВ
+
+        Matcher matcher1 = pattern1.matcher(s);
+        Matcher matcher2 = pattern2.matcher(s);
+        Matcher matcher3 = pattern3.matcher(s);
+        Matcher matcher4 = pattern4.matcher(s);
+        Matcher matcher5 = pattern5.matcher(s);
+
+        while (matcher1.find()) {
+            System.out.println("Position1: " + matcher1.start() + "     " + matcher1.group());
+        }
+        System.out.println("---------------------------");
+        while (matcher2.find()) {
+            System.out.println("Position2: " + matcher2.start() + "     " + matcher2.group());
+        }
+        System.out.println("---------------------------");
+        while (matcher3.find()) {
+            System.out.println("Position3: " + matcher3.start() + "     " + matcher3.group());
+        }
+        System.out.println("---------------------------");
+        while (matcher4.find()) {
+            System.out.println("Position4: " + matcher4.start() + "     " + matcher4.group());
+        }
+        System.out.println("---------------------------");
+        while (matcher5.find()) {
+            System.out.println("Position5: " + matcher5.start() + "     " + matcher5.group());
+        }
+
     }
 }
