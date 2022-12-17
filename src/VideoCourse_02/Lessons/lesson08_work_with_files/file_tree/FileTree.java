3ab7f0be90585e7fad8678184ba01cb89ccb080a
dgmedvedev <dg-medvedev@mail.ru>
2022-12-17 16:33:15 +0300

    change lesson09_regex.Regex2 (VideoCourse_02.Lessons)

--- src/VideoCourse_02/Lessons/lesson08_work_with_files/file_tree/FileTree.java
+++ src/VideoCourse_02/Lessons/lesson08_work_with_files/file_tree/FileTree.java
@@ -13,25 +13,25 @@ public class FileTree {
 
 class MyFileVisitor implements FileVisitor<Path> {
     @Override
-    public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
+    public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) {
         System.out.println("Enter to Directory: " + dir);
         return FileVisitResult.CONTINUE;
     }
 
     @Override
-    public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+    public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) {
         System.out.println("File name: " + file.getFileName());
         return FileVisitResult.CONTINUE;
     }
 
     @Override
-    public FileVisitResult visitFileFailed(Path file, IOException exc) throws IOException {
+    public FileVisitResult visitFileFailed(Path file, IOException exc) {
         System.out.println("Error while visiting file: " + file.getFileName());
         return FileVisitResult.TERMINATE;
     }
 
     @Override
-    public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
+    public FileVisitResult postVisitDirectory(Path dir, IOException exc) {
         System.out.println("Exit from Directory: " + dir);
         return FileVisitResult.CONTINUE;
     }
--- src/VideoCourse_02/Lessons/lesson09_regex/Regex2.java
+++ src/VideoCourse_02/Lessons/lesson09_regex/Regex2.java
@@ -11,8 +11,11 @@ public class Regex2 {
         Pattern pattern2 = Pattern.compile("[D78]"); // - соответствие одному любому символу из квадратных скобок
         Pattern pattern3 = Pattern.compile("AB[C-H][3-7]KP"); // - соответствие одному символу из диапазона
         Pattern pattern4 = Pattern.compile("AB[C-H3-7]KP"); // - соответствие одному символу из ДИАПАЗОНОВ
-        Pattern pattern5 = Pattern.compile("AB[^C-H3-7]KP"); // - знак ^ - соответствие одному символу НЕ из ДИАПАЗОНОВ
-        Pattern pattern6 = Pattern.compile("AB([D7])"); // - тритий символд D ИЛИ 7, также можно записать "AB(D|7)"
+        Pattern pattern5 = Pattern.compile("AB[^C-H3-7]KP"); // - знак ^ - соответствие одному символу, НЕ из ДИАПАЗОНОВ
+        Pattern pattern6 = Pattern.compile("AB(D|7)"); // - тритий символд D ИЛИ 7
+        Pattern pattern7 = Pattern.compile("AB."); // - любой символ, кроме символа новой строки
+        Pattern pattern8 = Pattern.compile("^ABD"); // - знак ^ не в [ ] - сотответствие выражению в начале строки
+        Pattern pattern9 = Pattern.compile("8KP$"); // - знак $ - сотответствие выражению в конце строки
 
         Matcher matcher1 = pattern1.matcher(s);
         Matcher matcher2 = pattern2.matcher(s);
@@ -20,7 +23,9 @@ public class Regex2 {
         Matcher matcher4 = pattern4.matcher(s);
         Matcher matcher5 = pattern5.matcher(s);
         Matcher matcher6 = pattern6.matcher(s);
-
+        Matcher matcher7 = pattern7.matcher(s);
+        Matcher matcher8 = pattern8.matcher(s);
+        Matcher matcher9 = pattern9.matcher(s);
 
         while (matcher1.find()) {
             System.out.println("Position1: " + matcher1.start() + "     " + matcher1.group());
@@ -44,6 +49,18 @@ public class Regex2 {
         System.out.println("---------------------------");
         while (matcher6.find()) {
             System.out.println("Position6: " + matcher6.start() + "     " + matcher6.group());
+        }
+        System.out.println("---------------------------");
+        while (matcher7.find()) {
+            System.out.println("Position7: " + matcher7.start() + "     " + matcher7.group());
+        }
+        System.out.println("---------------------------");
+        while (matcher8.find()) {
+            System.out.println("Position8: " + matcher8.start() + "     " + matcher8.group());
+        }
+        System.out.println("---------------------------");
+        while (matcher9.find()) {
+            System.out.println("Position9: " + matcher9.start() + "     " + matcher9.group());
         }
     }
 }
