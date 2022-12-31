190b51c213397a5c27961c55628167c30f11ac4f
dgmedvedev <dg-medvedev@mail.ru>
2022-12-31 12:58:20 +0300

    add lesson03; Homework:lesson02,03 (VideoCourse_03)

---
+++ src/VideoCourse_03/Homework/lesson02/MyFamilyNames.java
@@ -0,0 +1,18 @@
+package VideoCourse_03.Homework.lesson02;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MyFamilyNames {
+    public static void main(String[] args) {
+        List<String> listNames = new ArrayList<>(5);
+        listNames.add("Dima");
+        listNames.add("Oksana");
+        listNames.add("Maks");
+        listNames.add("Den");
+
+        for (String s : listNames) {
+            System.out.println(s);
+        }
+    }
+}
---
+++ src/VideoCourse_03/Lessons/lesson03/Days.java
@@ -0,0 +1,15 @@
+package VideoCourse_03.Lessons.lesson03;
+
+public class Days {
+    public static void main(String[] args) {
+        int days = 5000;
+        int years = days / 365;
+        int month = (days % 365) / 31;
+        int leftDays = days - years * 365 - month * 31;
+
+        System.out.println("Общее кол-во дней: " + days);
+        System.out.println("Лет: " + years);
+        System.out.println("Месяцев: " + month);
+        System.out.println("Дней: " + leftDays);
+    }
+}
