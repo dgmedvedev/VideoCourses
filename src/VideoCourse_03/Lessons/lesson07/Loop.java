273f47102037f122749aec5e0b39d93ef2225b3c
dgmedvedev <dg-medvedev@mail.ru>
2023-01-03 13:13:22 +0300

    add Lessons lesson07 (VideoCourse_03)

---
+++ src/VideoCourse_03/Lessons/lesson07/Loop.java
@@ -0,0 +1,14 @@
+package VideoCourse_03.Lessons.lesson07;
+
+public class Loop {
+    public static void main(String[] args) {
+        int sum = 0;
+        double avgGrade;
+        for (int i = 0; i <= 100; i++) {
+            sum += i;
+        }
+        avgGrade = sum / 100.0;
+        System.out.println("sum = " + sum);
+        System.out.println(avgGrade);
+    }
+}
