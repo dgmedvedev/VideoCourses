0d09b8bc9eee898d5429ba24230de89cd4563ba2
dgmedvedev <dg-medvedev@mail.ru>
2023-01-06 22:03:56 +0300

    add Lessons,Homework:lesson09 (VideoCourse_03)

--- src/VideoCourse_03/Homework/lesson09/Task1.java
+++ src/VideoCourse_03/Homework/lesson09/Task1.java
@@ -6,12 +6,13 @@ public class Task1 {
         seasons("March");
         seasons("12");
     }
-    static void seasons(String month){
-        switch (month){
-            case "December","January","February" -> System.out.println("winter");
-            case "March","April","May" -> System.out.println("spring");
-            case "June","July","August" -> System.out.println("summer");
-            case "September","October","November" -> System.out.println("autumn");
+
+    static void seasons(String month) {
+        switch (month) {
+            case "December", "January", "February" -> System.out.println("winter");
+            case "March", "April", "May" -> System.out.println("spring");
+            case "June", "July", "August" -> System.out.println("summer");
+            case "September", "October", "November" -> System.out.println("autumn");
             default -> System.out.println("There is no such month");
         }
     }
