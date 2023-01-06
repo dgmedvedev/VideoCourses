0a9759d726edc012c6740e4a846bff2aa9a2d7d4
dgmedvedev <dg-medvedev@mail.ru>
2023-01-06 20:32:12 +0300

    change Lessons:lesson08 (VideoCourse_03)

--- src/VideoCourse_03/Lessons/lesson08/Task1.java
+++ src/VideoCourse_03/Lessons/lesson08/Task1.java
@@ -3,12 +3,12 @@ package VideoCourse_03.Lessons.lesson08;
 public class Task1 {
     public static void main(String[] args) {
         int[] firstArray = new int[901];
-        for (int i = 100, j = 0; i < 1001; i++, j++) {
-            firstArray[j] = i;
+        for (int i = 0; i < firstArray.length; i++) {
+            firstArray[i] = i + 100;
         }
         int[] secondArray = new int[firstArray.length];
-        for (int i = 1000, j = 1; j <= firstArray.length; i--, j++) {
-            secondArray[j - 1] = firstArray[firstArray.length - j];
+        for (int i = 0, j = firstArray.length - 1; i < firstArray.length; i++, j--) {
+            secondArray[i] = firstArray[j];
         }
 
         for (int i : secondArray) {
