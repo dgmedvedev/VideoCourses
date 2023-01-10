381d5d1a44576e4289c150e31b9af8c4b1713c88
dgmedvedev <dg-medvedev@mail.ru>
2023-01-10 23:27:55 +0300

    add Homework:lesson20,21 (VideoCourse_03)

--- src/VideoCourse_03/Homework/lesson15/Box.java
+++ src/VideoCourse_03/Homework/lesson15/Box.java
@@ -1,32 +1,53 @@
 package VideoCourse_03.Homework.lesson15;
 
 public class Box {
-    private final double WIDTH;
-    private final double HEIGHT;
-    private final double LENGTH;
+    private double width;
+    private double height;
+    private double length;
 
-    public double getWIDTH() {
-        return WIDTH;
+    public double getWidth() {
+        return width;
     }
 
-    public Box(double WIDTH, double HEIGHT, double LENGTH) {
-        this.WIDTH = WIDTH;
-        this.HEIGHT = HEIGHT;
-        this.LENGTH = LENGTH;
+    public double getHeight() {
+        return height;
+    }
+
+    public double getLength() {
+        return length;
+    }
+
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public void setLength(double length) {
+        this.length = length;
+    }
+
+    public Box(double width, double height, double length) {
+        this.width = width;
+        this.height = height;
+        this.length = length;
     }
 
     public Box(Box box1, Box box2) {
-        this.WIDTH = box1.WIDTH + box2.WIDTH;
-        this.HEIGHT = box1.HEIGHT + box2.HEIGHT;
-        this.LENGTH = box1.LENGTH + box2.LENGTH;
+        this.width = box1.width + box2.width;
+        this.height = box1.height + box2.height;
+        this.length = box1.length + box2.length;
     }
 
     public double volume() {
-        return WIDTH * HEIGHT * LENGTH;
+        return width * height * length;
     }
 
     public Box concatBox(Box box) {
-        return new Box(WIDTH + box.WIDTH, HEIGHT + box.HEIGHT, LENGTH + box.LENGTH);
+        return new Box(width + box.width, height + box.height, length + box.length);
     }
 }
 
@@ -37,14 +58,16 @@ class BoxTest {
         Box box3 = box1.concatBox(box2);
         Box box4 = new Box(box2, box3);
 
-        System.out.println(box1.getWIDTH());
-        System.out.println(box2.getWIDTH());
-        System.out.println(box3.getWIDTH());
-        System.out.println(box4.getWIDTH());
-        System.out.println("--------");
         System.out.println(box1.volume());
         System.out.println(box2.volume());
         System.out.println(box3.volume());
         System.out.println(box4.volume());
+        System.out.println("--------");
+        System.out.println(box1.getWidth());
+        System.out.println(box2.getHeight());
+        System.out.println(box3.getLength());
+        box1.setWidth(3);
+        box2.setHeight(4);
+        box3.setLength(5);
     }
 }
