bd71a2e610de47c092589a0ee351ed26f7cb62d9
dgmedvedev <dg-medvedev@mail.ru>
2023-01-15 19:57:05 +0300

    The end (VideoCourse_03)

---
+++ src/VideoCourse_02/Homework/Task.java
@@ -0,0 +1,7 @@
+package VideoCourse_02.Homework;
+
+public class Task {
+    public static void main(String[] args) {
+        System.out.println("There are no homework assignments in this video course");
+    }
+}
