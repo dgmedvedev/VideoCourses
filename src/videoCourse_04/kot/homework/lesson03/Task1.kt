7d13e0a96a543fbfd0b48193c991bc628a9abbed
dgmedvedev <dg-medvedev@mail.ru>
2023-01-17 13:17:44 +0300

    add Homework:lesson03 (VideoCourse_04)

---
+++ src/videoCourse_04/kot/homework/lesson03/Task1.kt
@@ -0,0 +1,14 @@
+package videoCourse_04.kot.homework.lesson03
+
+private val str1: String? = "null"
+private val str1Length: Int = str1?.length ?: 0
+private val str2: String? = null
+private val str2Length: Int = str2?.length ?: 0
+private val str3: String? = null
+private val str3Length: Int = str3?.length ?: 0
+
+
+fun main() {
+    val stringsLength: Int = str1Length + str2Length + str3Length
+    println(stringsLength)
+}
