ab32e6f35d8343d64933688cf8e4dfc1dee5aab5
dgmedvedev <dg-medvedev@mail.ru>
2023-02-05 18:45:36 +0300

    add lessons.lesson401 (videoCourse_04)

---
+++ src/videoCourse_04/kot/lessons/lesson401/Main.kt
@@ -0,0 +1,7 @@
+package videoCourse_04.kot.lessons.lesson401
+
+fun main() {
+    val user = User()
+    user.age = 15
+    user.name = "Andrej"
+}
---
+++ src/videoCourse_04/kot/lessons/lesson401/User.kt
@@ -0,0 +1,6 @@
+package videoCourse_04.kot.lessons.lesson401
+
+class User {
+    var age: Int = 0
+    var name: String? = null
+}
