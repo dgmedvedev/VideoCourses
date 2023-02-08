07563fb3d7657f042fbfddb783362265c2b33a1e
dgmedvedev <dg-medvedev@mail.ru>
2023-02-08 13:16:08 +0300

    change lessons.lesson403 (VideoCourse_04)

--- src/videoCourse_04/kot/lessons/lesson403/Cat.kt
+++ src/videoCourse_04/kot/lessons/lesson403/Cat.kt
@@ -1,10 +1,16 @@
 package videoCourse_04.kot.lessons.lesson403
 
-class Cat(val name: String, val age: Int, val weight: Float = 0F) {
+class Cat(val name: String, val age: Int, private val weight: Float = 0F) {
+
+    val isOld: Boolean
+        get() = age >= 12
+
     override fun toString(): String {
         return "Cat(name='$name', age=$age, weight=$weight)"
     }
 
-    // Вместо метода, указанного ниже, в классе CatTest создали extension функцию
+    // Вместо метода, указанного ниже, в классе CatTest создали extension функцию,
+    // но если функция по сути ничего не делает, а показывает какое-то св-во,
+    // то лучше создать переменную (val isOld), и переопределить геттер
     //fun isOld() = age >= 12
 }
--- src/videoCourse_04/kot/lessons/lesson403/CatTest.kt
+++ src/videoCourse_04/kot/lessons/lesson403/CatTest.kt
@@ -3,7 +3,7 @@ package videoCourse_04.kot.lessons.lesson403
 fun main() {
     val cat = Cat("Barsik", 5, 3f)
     println(cat.toString())
-    println(cat.isOld())
+    println(cat.isOld)
 }
 
-fun Cat.isOld() = age >= 12
+//fun Cat.isOld() = age >= 12
